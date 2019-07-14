1a76d9d5b14214c62eb82f1c1295b27d75867b97
Graham Fairweather <xotic750@gmail.com>
2019-07-14 23:46:01 +0200

    :bookmark: v4.0.15
    Auto build

--- dist/normalize-space-x.js
+++ dist/normalize-space-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-13T23:37:48.826Z",
+  "date": "2019-07-14T21:45:57.281Z",
   "describe": "",
   "description": "Trims and replaces sequences of whitespace characters by a single space.",
   "file": "normalize-space-x.js",
   "hash": "e8e70b1c1a798e7c94a7",
   "license": "MIT",
-  "version": "4.0.14"
+  "version": "4.0.15"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
