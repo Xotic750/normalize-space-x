a443ef211a6e3ecaee00ddc29f72e57e38a0bd3e
Graham Fairweather <xotic750@gmail.com>
2019-08-20 10:54:40 +0200

    Maintenance

--- dist/normalize-space-x.js
+++ dist/normalize-space-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-18T13:58:49.291Z",
+  "date": "2019-08-20T08:54:33.961Z",
   "describe": "",
   "description": "Trims and replaces sequences of whitespace characters by a single space.",
   "file": "normalize-space-x.js",
